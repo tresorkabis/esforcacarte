d99960760a68435a83b8729717a6a9cd83e51e64
Tresor <tresorkabis@gmail.com>
2025-03-08 23:03:22 +0100

    maj

--- settings/prod.py
+++ settings/prod.py
@@ -1,11 +1,10 @@
 from .base import *
+import dj_database_url
+import os
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
